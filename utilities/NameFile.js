069f9efdf0e2a3243dfb86e99203f1f9c475c657
Petro Tkachuk <petrotkachuk6@gmail.com>
2026-04-14 16:29:47 +0200

    Update NameFile.js

--- utilities/NameFile.js
+++ utilities/NameFile.js
@@ -1,10 +1,12 @@
-const d = new Date();
+const getDataFormattata = () => {
+  const d = new Date();
+  const giorno = String(d.getDate()).padStart(2, '0');
+  const mese = String(d.getMonth() + 1).padStart(2, '0');
+  const anno = d.getFullYear();
+  const ore = String(d.getHours()).padStart(2, '0');
+  const minuti = String(d.getMinutes()).padStart(2, '0');
 
-// Formattazione manuale con padStart per avere sempre due cifre (es. 05 invece di 5)
-const giorno = String(d.getDate()).padStart(2, '0');
-const mese = String(d.getMonth() + 1).padStart(2, '0');
-const anno = d.getFullYear();
-const ore = String(d.getHours()).padStart(2, '0');
-const minuti = String(d.getMinutes()).padStart(2, '0');
+  return `${giorno}.${mese}.${anno}_${ore}.${minuti}`;
+};
 
-export const dataFormattata = `${giorno}.${mese}.${anno}_${ore}.${minuti}`;
+module.exports = { getDataFormattata };
